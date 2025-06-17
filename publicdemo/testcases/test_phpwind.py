8851c560db48256dd4d36f3ef567eb1378b58fbb
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-06-17 19:18:44 -0400

    test phpwind

---
+++ publicdemo/testcases/test_phpwind.py
@@ -0,0 +1,29 @@
+from publicdemo.commons.request_util import RequestUtil
+import re
+
+class TestPhpwind:
+    csrf_token = ""
+
+    # cookie related interface
+    def test_start(self):
+        url = "http://47.107.116.139/phpwind/"
+        res = RequestUtil().all_send_request(method="get",url=url)
+        result = res.text
+        TestPhpwind.csrf_token = re.search('name="csrf_token" value="(.*?)"', result).group(1)
+        print(TestPhpwind.csrf_token)
+
+    def test_login(self):
+        url = "http://47.107.116.139/phpwind/index.php?m=u&c=login&a=dorun"
+        datas = {
+            "username": "zhouy218",
+            "password": "136671",
+            "csrf_token": TestPhpwind.csrf_token,
+            "back_url": "http://47.107.116.139/phpwind/",
+            "invite": ""
+        }
+        headers = {
+            "Accept": "application/json, text/javascript, /; q=0.01",
+            "X-Requested-With": "XMLHttpRequest"
+        }
+        res = RequestUtil().all_send_request(method="post",url=url, data=datas, headers=headers)
+        print(res.json())
