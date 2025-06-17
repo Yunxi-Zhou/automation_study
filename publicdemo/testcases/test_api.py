4cad3af1350c14ddc24ddf45e03aa407ecbf513a
Yunxi-Zhou <zhouyunxi1115@gmail.com>
2025-06-17 19:17:56 -0400

    test api

--- publicdemo/testcases/test_api.py
+++ publicdemo/testcases/test_api.py
@@ -1,12 +1,11 @@
 import jsonpath
-import requests
 import random
 import json
-import re
+from publicdemo.commons.request_util import RequestUtil
+
 
 class TestApi:
     access_token = ""
-    csrf_token = ""
 
     # 1. get the access token Interface
     def test_get_token(self):
@@ -16,8 +15,9 @@ class TestApi:
             "appid": "wx74a8627810cfa308",
             "secret": "e40a02f9d79a8097df497e6aaf93ab80"
         }
-        res = requests.get(url, params=datas)
+        res = RequestUtil().all_send_request(method="get",url=url,params=datas)
         result = res.json()
+        print(result)
         # get access token (method 1: use global value to store and get the value)
         value = jsonpath.jsonpath(result, "$.access_token")
         TestApi.access_token = value[0]
@@ -28,7 +28,7 @@ class TestApi:
         datas = {
             "access_token": TestApi.access_token
         }
-        res = requests.get(url, params=datas)
+        res = RequestUtil().all_send_request(method="get",url=url,params=datas)
 
     # 3. create tag API InterFace (post)
     def test_create_flag(self):
@@ -36,43 +36,12 @@ class TestApi:
         datas = {
             "tag":{"name":"广东"+str(random.randint(10000,99999))}
         }
-        res = requests.post(url, json=datas)
+        res = RequestUtil().all_send_request(method="post",url=url, json=datas)
         result_str = json.loads(json.dumps(res.json()).replace("\\\\","\\"))
 
     # 4. delete file
     def test_file_upload(self):
         url = "https://api.weixin.qq.com/cgi-bin/media/upload?acpi.access_token" + TestApi.access_token
-        datas = {"media": open("/Users/ethan/Downloads/IMG_2049.jpg", "rb")}
-        res = requests.post(url, json=datas)
+        datas = {"media": open("C:\\Users\zhouy\Downloads\IMG_2049.jpg", "rb")}
+        res = RequestUtil().all_send_request(method="post",url=url, files=datas)
         print(res.json())
-
-    # cookie related interface
-    def test_start(self):
-        url = "http://47.107.116.139/phpwind/"
-        res = requests.get(url)
-        result = res.text
-        print(result)
-        TestApi.csrf_token = re.search('name="csrf_token" value="(.*?)"', result).group(1)
-
-    def test_login(self):
-        url = "http://47.107.116.139/phpwind/index.php?m=u&c=login&a=dorun"
-        datas = {
-            "username": "zhouy218",
-            "password": "136671",
-            "csrf_token": read_yaml("csrf_token"),
-            "back_url": "http://47.107.116.139/phpwind/",
-            "invite": ""
-        }
-        headers = {
-            "Accept": "application/json, text/javascript, /; q=0.01",
-            "X-Requested-With": "XMLHttpRequest"
-        }
-        res = requests.post(url, json=datas, headers=headers)
-        print(res.json())
-
-if __name__ == '__main__':
-    test = TestApi()
-    test.test_get_token()
-    test.test_select_flag()
-    test.test_file_upload()
-    test.test_start()
