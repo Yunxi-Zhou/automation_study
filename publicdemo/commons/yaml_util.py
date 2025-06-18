b6c6d55d07141dae692b79c8686d23c9a77362e8
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-06-18 17:06:41 -0400

    write,read,clear yaml

---
+++ publicdemo/commons/yaml_util.py
@@ -0,0 +1,18 @@
+import os
+import yaml
+
+# write
+def write_yaml(data):
+    with open(os.getcwd()+"/extract.yaml",encoding="utf-8",mode="a+") as f:
+        yaml.dump(data,stream=f,allow_unicode=True)
+
+# read
+def read_yaml(key):
+    with open(os.getcwd()+"/extract.yaml",encoding="utf-8",mode="r") as f:
+        value = yaml.load(f,yaml.FullLoader)
+        return value[key]
+
+# clear
+def clear_yaml():
+    with open(os.getcwd()+"/extract.yaml",encoding="utf-8",mode="w") as f:
+        f.truncate()
