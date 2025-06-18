bc20b0454bda3b74bb3a6d5ebb9db06e04ba5b1c
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-06-18 17:06:07 -0400

    all send request enc

--- publicdemo/commons/request_util.py
+++ publicdemo/commons/request_util.py
@@ -4,6 +4,7 @@ class RequestUtil:
     sess = requests.Session()
 
     # common send requests
+    # Connection: keep-alive
     def all_send_request(self, **kwargs):
         res = RequestUtil.sess.request(**kwargs)
         return res
