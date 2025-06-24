4491d46b491dc30d2739f2c8fe56bda3b7df7afb
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-06-24 10:51:29 -0400

    function for yaml

--- publicdemo/commons/request_util.py
+++ publicdemo/commons/request_util.py
@@ -7,4 +7,5 @@ class RequestUtil:
     # Connection: keep-alive
     def all_send_request(self, **kwargs):
         res = RequestUtil.sess.request(**kwargs)
+        print("返回内容：",res.text)
         return res
