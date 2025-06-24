304c4e5bf2cc5f0a7367fb48b487b1fd8bb71e98
Yunxi-Zhou <161828903+Yunxi-Zhou@users.noreply.github.com>
2025-06-24 10:52:05 -0400

    functions for yaml

--- publicdemo/commons/yaml_util.py
+++ publicdemo/commons/yaml_util.py
@@ -16,3 +16,9 @@ def read_yaml(key):
 def clear_yaml():
     with open(os.getcwd()+"/extract.yaml",encoding="utf-8",mode="w") as f:
         f.truncate()
+
+# read testcase
+def read_testcase(path):
+     with open(path,encoding="utf-8",mode="r") as f:
+        value = yaml.load(f,yaml.FullLoader)
+        return value
